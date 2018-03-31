25ee13ba98073b23bd435649446ecb9f6b51ceb7
Trent Scholl <me@trentscholl.com>
2018-03-31 23:11:31 +1000

    Changes to support ES6

--- Miscellaneous/docker/CHANGES.md
+++ Miscellaneous/docker/CHANGES.md
@@ -2,7 +2,7 @@
 TODO
 
 1. Static content?
-1. MEtricbeat reports its host for system modeul as "metricbeat" - change to X?
+1. Metricbeat reports its host for system module as "metricbeat" - change to X?
 1. Docker-logs, apache dashboards?
 1. Dynamic loading of config - beats and LS.
 1. What does windows do with the /var/log entry? use the VMS
--- Miscellaneous/docker/full_stack_example/init/configure-stack.sh
+++ Miscellaneous/docker/full_stack_example/init/configure-stack.sh
@@ -1,39 +1,16 @@
 #!/bin/bash
 
-
 # Wait for Elasticsearch to start up before doing anything.
-until curl -u elastic:changeme -s http://elasticsearch:9200/_cat/health -o /dev/null; do
+until curl -u elastic:${ES_PASSWORD} -s http://elasticsearch:9200/_cat/health -o /dev/null; do
     echo Waiting for Elasticsearch...
     sleep 1
 done
-echo "Setting password to ${ES_PASSWORD}"
-curl -s -XPUT -u elastic:changeme 'elasticsearch:9200/_xpack/security/user/elastic/_password' -H "Content-Type: application/json" -d "{
-  \"password\" : \"${ES_PASSWORD}\"
-}"
-
-curl -s -XPUT -u elastic:${ES_PASSWORD} 'elasticsearch:9200/_xpack/security/user/kibana/_password' -H "Content-Type: application/json" -d "{
-  \"password\" : \"${ES_PASSWORD}\"
-}"
-
-curl -s -XPUT -u elastic:${ES_PASSWORD} 'elasticsearch:9200/_xpack/security/user/logstash_system/_password' -H "Content-Type: application/json" -d "{
-  \"password\" : \"${ES_PASSWORD}\"
-}"
-
 
 # Wait for Kibana to start up before doing anything.
 until curl -s http://kibana:5601/login -o /dev/null; do
     echo Waiting for Kibana...
     sleep 1
 done
-
-# Import the standard Beats dashboards.
-/usr/share/metricbeat/scripts/import_dashboards \
-  -beat '' \
-  -file /usr/share/metricbeat/beats-dashboards-${ELASTIC_VERSION}.zip \
-  -es http://elasticsearch:9200 \
-  -user elastic \
-  -pass ${ES_PASSWORD}
-
 
 # Set the default index pattern.
 curl -s -XPUT http://elastic:${ES_PASSWORD}@elasticsearch:9200/.kibana/config/${ELASTIC_VERSION} \
@@ -57,7 +34,7 @@ do
      template_id="${filename%.*}"
      echo "Loading $template_id template..."
      curl -s  -H 'Content-Type: application/json' -XPUT http://elastic:${ES_PASSWORD}@elasticsearch:9200/_template/$template_id -d@$f
-     #We assume we want an index pattern in kibana
+     # We assume we want an index pattern in kibana
      curl -s -XPUT http://elastic:${ES_PASSWORD}@elasticsearch:9200/.kibana/index-pattern/$template_id-* \
      -d "{\"title\" : \"$template_id-*\",  \"timeFieldName\": \"@timestamp\"}"
 done
